87b58a912f5a6b1c4ee8b3a63ede05d11cd8ff91
863056768 <a863056768@gmail.com>
2024-06-12 07:39:39 +0800

    Update

--- transmission/Transmission-ScrollBar.user.js
+++ transmission/Transmission-ScrollBar.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Transmission-ScrollBar
 // @namespace    http://tampermonkey.net/
-// @version      0.1.1
+// @version      0.1.2
 // @description  try to take over the world!
 // @author       Hueizhi
 // @match        http://orangepizero2:9091/transmission/web/
