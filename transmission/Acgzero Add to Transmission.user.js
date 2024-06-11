8ba571c52e2a867078efea490e9e9466f7c25db3
863056768 <a863056768@gmail.com>
2024-06-12 07:36:30 +0800

    Update

--- transmission/Acgzero Add to Transmission.user.js
+++ transmission/Acgzero Add to Transmission.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         Acgzero Add to Transmission
 // @namespace    http://tampermonkey.net/
-// @version      0.1.1
+// @version      0.1.2
 // @description  try to take over the world!
 // @author       Hueizhi
-// @match        https://bt.acgzero.com/?keyword=%*
+// @match        https://bt.acgzero.com/?keyword=*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=acgzero.com
 // @downloadURL  https://github.com/863056768/user-script/raw/main/transmission/Acgzero%20Add%20to%20Transmission.user.js
 // @updateURL    https://github.com/863056768/user-script/raw/main/transmission/Acgzero%20Add%20to%20Transmission.user.js
