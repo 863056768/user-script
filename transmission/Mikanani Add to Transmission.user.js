7284b71301e4f59ef0b6ad340ffbc34bfc3de3f5
863056768 <a863056768@gmail.com>
2025-05-23 22:38:20 +0800

    fix: canot install button on table

--- transmission/Mikanani Add to Transmission.user.js
+++ transmission/Mikanani Add to Transmission.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Mikanani.me Add to Transmission
 // @namespace    http://tampermonkey.net/
-// @version      0.1.5
+// @version      0.1.6
 // @description  try to take over the world!
 // @author       Hueizhi
 // @match        https://mikanani.me/*
@@ -243,7 +243,7 @@ const torrentBtnEventHandler = newBtnEventHandler(
 
 function installButtonOnTable() {
   const installButton = (table) => {
-    if (table.querySelectorAll("thead>tr>th").length >= 5) {
+    if ([...table.querySelectorAll("thead > tr > th")].some(th => th.textContent.trim() === "添加")) {
       return;
     }
     const tbody = table.querySelector("tbody");
@@ -251,6 +251,7 @@ function installButtonOnTable() {
     th.textContent = "添加";
     table.querySelector("thead>tr").prepend(th);
     tbody.querySelectorAll("tr").forEach((tr) => {
+      console.log(tr.querySelector("a.magnet-link-wrap").textContent);
       // const magnet = tr.querySelector(".magnet-link").getAttribute("data-clipboard-text")
       // const url = tr.querySelector("td:nth-child(5)>a").href
       const td = document.createElement("td");
@@ -264,7 +265,6 @@ function installButtonOnTable() {
 
       td.append(magnetBtn, torrentBtn);
       tr.prepend(td);
-
       magnetBtn.addEventListener("click", magnetBtnEventHandler);
       torrentBtn.addEventListener("click", torrentBtnEventHandler);
     });
